f48b0a23fd388dfcf7904a0852ee17b1333854cc
zmacconnell <zachary.macconnell@gmail.com>
2022-10-10 17:49:57 -0600

    Updated Seeker

--- Game/Seeker.cs
+++ Game/Seeker.cs
@@ -1,40 +1,43 @@
 using System;
+using System.Collections.Generic;
 
 
 namespace Unit03.Game
 {
-    // TODO: Implement the Seeker class as follows...
+    /// <summary>
+    /// <para>The person looking for the Hider.</para>
+    /// <para>
+    /// The responsibility of a Seeker is to keep track of its location.
+    /// </para>
+    /// </summary>
+    public class Seeker
+    {
+        Random rnd = new Random();
+        int _location = rnd.Next(1001);
 
-    // 1) Add the class declaration. Use the following class comment.
-
-        /// <summary>
-        /// <para>The person looking for the Hider.</para>
-        /// <para>
-        /// The responsibility of a Seeker is to keep track of its location.
-        /// </para>
-        /// </summary>
-
-
-    // 2) Create the class constructor. Use the following method comment.
-        
         /// <summary>
         /// Constructs a new instance of Seeker.
         /// </summary>
-       
+        public Seeker()
+        {
+        }
 
-    // 3) Create the GetLocation() method. Use the following method comment.
-        
         /// <summary>
         /// Gets the current location.
         /// </summary>
         /// <returns>The current location as an int.</returns>
-        
+        public int GetLocation()
+        {
+            return _location;
+        }
 
-    // 4) Create the MoveLocation(int location) method. Use the following method comment.
-        
         /// <summary>
         /// Moves to the given location.
         /// </summary>
         /// <param name="location">The given location.</param>
-    
+        public void MoveLocation(int location)
+        {
+            _location = location;
+        }
+    }
 }
