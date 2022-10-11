fdce582c00bc8e710dd4ef9584123bc16d0774be
zmacconnell <zachary.macconnell@gmail.com>
2022-10-11 10:34:20 -0600

    Changed location to private

--- Game/Seeker.cs
+++ Game/Seeker.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 
 
 namespace Unit03.Game
@@ -12,7 +11,7 @@ namespace Unit03.Game
     /// </summary>
     public class Seeker
     {
-        int _location = 0;
+        private int _location = 0;
 
         /// <summary>
         /// Constructs a new instance of Seeker.
